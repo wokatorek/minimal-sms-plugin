0acc8a8124776b7ffd2f2fc58e2b08eb2974a1ea
Michał Wlaź <michal.wlaz@orange.com>
2017-05-09 13:48:52 +0200

    init commit

---
+++ src/android/MinimalSMSPlugin.java
@@ -0,0 +1,30 @@
+package pl.wokatorek.cordova.plugin.sms;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MinimalSMSPlugin extends CordovaPlugin {
+
+  @Override
+  public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+    if (action.equals("echo")) {
+      String message = args.getString(0);
+      this.echo(message, callbackContext);
+      return true;
+    }
+    return false;
+  }
+
+  private void echo(String message, CallbackContext callbackContext) {
+    if (message != null && message.length() > 0) {
+      callbackContext.success(message);
+    } else {
+      callbackContext.error("Expected one non-empty string argument.");
+    }
+  }
+
+}
