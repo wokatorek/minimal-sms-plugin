227b5810af7585b8c40a6babde54e79849251e83
Michał Wlaź <michal.wlaz@orange.com>
2017-05-11 14:07:11 +0200

    cur2json

--- src/android/MinimalSMSPlugin.java
+++ src/android/MinimalSMSPlugin.java
@@ -95,9 +95,36 @@ public class MinimalSMSPlugin extends CordovaPlugin {
         builder.append(", ");
     }
     String str = builder.toString();
-    callbackContext.success("Columns:"+str);
+    // callbackContext.success("Columns:"+str);
+    callbackContext.success(cur2Json(cursor));
     cursor.close();
     return null;
+  }
+
+  public JSONArray cur2Json(Cursor cursor) {
+
+    JSONArray resultSet = new JSONArray();
+    cursor.moveToFirst();
+    while (cursor.isAfterLast() == false) {
+        int totalColumn = cursor.getColumnCount();
+        JSONObject rowObject = new JSONObject();
+        for (int i = 0; i < totalColumn; i++) {
+            if (cursor.getColumnName(i) != null) {
+                try {
+                    rowObject.put(cursor.getColumnName(i),
+                            cursor.getString(i));
+                } catch (Exception e) {
+                    Log.d(TAG, e.getMessage());
+                }
+            }
+        }
+        resultSet.put(rowObject);
+        cursor.moveToNext();
+    }
+
+    cursor.close();
+    return resultSet;
+
   }
 
   private PluginResult startListeningAction(boolean isIntercepting, CallbackContext callbackContext){
