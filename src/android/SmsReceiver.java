e791a27ff84a5d2d9d0faaebc7e68d7ec1b189f9
Michał Wlaź <michal.wlaz@orange.com>
2017-05-12 08:04:26 +0200

    listening for sms received fix #8 - import Log

--- src/android/SmsReceiver.java
+++ src/android/SmsReceiver.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.telephony.SmsMessage;
+import android.util.Log;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
